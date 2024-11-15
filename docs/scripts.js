75fc1d9b8ac46d43acbef1c3ea8c857435a84646
Jjordan <joesanz182@gmail.com>
2024-11-15 13:20:32 -0500

    Corregir funcionalidad de búsqueda

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,13 +1,15 @@
-document.getElementById('search').addEventListener('input', function() {
-    let filter = this.value.toLowerCase();
-    let songs = document.querySelectorAll('.song');
-
-    songs.forEach(function(song) {
-        let title = song.querySelector('h2').textContent.toLowerCase();
-        if (title.indexOf(filter) > -1) {
-            song.style.display = '';
-        } else {
-            song.style.display = 'none';
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('search').addEventListener('input', function() {
+        let filter = this.value.toLowerCase();
+        let songs = document.querySelectorAll('.song');
+    
+        songs.forEach(function(song) {
+            let title = song.querySelector('h2').textContent.toLowerCase();
+            if (title.indexOf(filter) > -1) {
+                song.style.display = '';
+            } else {
+                song.style.display = 'none';
+            }
+        });
     });
 });
