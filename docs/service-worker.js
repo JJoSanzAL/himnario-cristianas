ec6e77a466ba2e1466e3ffbfbcb318481d596d91
Jjordan <joesanz182@gmail.com>
2025-08-14 16:12:26 -0500

    Corregida la ruta del Service Worker para GitHub Pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('himnario-cache').then((cache) => {
       return cache.addAll([
-        '/docs/',
-        '/docs/index.html',
-        '/docs/styles.css',
-        '/docs/scripts.js',
-        // Lista de archivos adicionales que quieras cachear
+        './',
+        './index.html',
+        './styles.css',
+        './scripts.js',
+        './manifest.json',
       ]);
     })
   );
